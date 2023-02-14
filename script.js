b853187926b5780239fbf655acc769cd059e2148
Jeevana Mantina <113972063+Jeevanamsk@users.noreply.github.com>
2023-02-14 14:04:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function swapTheme(){
-            const button=document.getElementById("swap");
-            const targetElement = document.getElementById('app');
-            button.addEventListener('click', function() {
-                targetElement.style.backgroundColor = 'black';
-            })
-        }
+function swapTheme() {
+  const app = document.getElementById('app');
+  const swapBtn = document.getElementById('swap');
+  
+  // Toggle "day" and "night" classes of app div
+  app.classList.toggle('day');
+  app.classList.toggle('night');
+  
+  // Toggle "button_day" and "button_night" classes of swap button
+  swapBtn.classList.toggle('button_day');
+  swapBtn.classList.toggle('button_night');
+}
+
 
